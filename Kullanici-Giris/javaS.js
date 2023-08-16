bb7dfc9d04cdb3b77a16774b31fe098f2aac6fd3
Erkut Elik <erkutelik@gmail.com>
2023-08-16 16:18:22 +0300

    Placeholder Json: Veri çekme işlemi başarılı. Cektigi verileri web sitesinden kontrol ettiğimde web sitesinden o veri bulunmamakta veriler isime gore cekiliyor fakat isim degeri sitede yok

--- Kullanici-Giris/javaS.js
+++ Kullanici-Giris/javaS.js
@@ -25,16 +25,25 @@ var mailTb = document.querySelector('#mailTb');
 var sifreTb = document.querySelector('#sifreTb');
 document.querySelector('#button').addEventListener('click', button);
 var mesajElement =document.querySelector('#mesaj')
-
+var place=document.querySelector('#PlaceHolder')
 function button() {
     if (mailTb.value === 'admin@erkut.com' && sifreTb.value === '123') {
         console.log('Doğru Giriş');
-        mesaj('Doğru Giriş Yaptınız👍')
+        mesaj('Doğru Giriş Yaptınız👍');
     } else {
         console.log('Yanlış bilgiler');
-        mesaj('lütfen bilgileriniz kontrol ediniz❗')
+        mesaj('Lütfen bilgilerinizi kontrol ediniz❗');
+        
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then((json) => {
+            const adlar = json.map(user => user.username); // Sadece "ad" kısmını al
+            console.log(adlar);
+        })
+
     }
 }
+
 
 function mesaj(mesaj_){
     mesajElement.textContent = mesaj_; // Değişken adı güncellendi
