370dff38714f02d3c30dd4d81406a7f803899bb2
Erkut Elik <erkutelik@gmail.com>
2023-08-16 22:05:46 +0300

    Mesaj Alani:Js.Notify ile  Düzeltildi

--- Kullanici-Giris/javaS.js
+++ Kullanici-Giris/javaS.js
@@ -1,50 +1,59 @@
-window.addEventListener('load', function() {
+function sagKutu(bilgi,durum){
+    $.notify(bilgi, {
+        position: "right-bott",
+        className: durum
+    });
+    
+
+}
+
+
+      
+
+
+
+window.addEventListener('load', function () {
     if (!window.Notification) return;
 });
 
-const sendNotification=(permission)=>{
+const sendNotification = (permission) => {
 
-    let Notification_=new Notification('yeni bildirim',{
-        body:'Hoşgeldiniz',
-        icon:'LinkBicimlendirmesi.png'
+    let Notification_ = new Notification('yeni bildirim', {
+        body: 'Hoşgeldiniz',
+        icon: 'LinkBicimlendirmesi.png'
     })
     console.log(Notification_)
 
 }
 
-const buttons = document.querySelector('#button');
-buttons.addEventListener('click', () => {
-    alert('Buttona basıldı')
-    Notification
-        .requestPermission()
-        .then(sendNotification);
-        //Bildirim gönder buttonu
-});
+
 
 var mailTb = document.querySelector('#mailTb');
 var sifreTb = document.querySelector('#sifreTb');
 document.querySelector('#button').addEventListener('click', button);
-var mesajElement =document.querySelector('#mesaj')
-var place=document.querySelector('#PlaceHolder')
+var mesajElement = document.querySelector('#mesaj')
+var place = document.querySelector('#PlaceHolder')
+
 function button() {
     if (mailTb.value === 'admin@erkut.com' && sifreTb.value === '123') {
-        console.log('Doğru Giriş');
-        mesaj('Doğru Giriş Yaptınız👍');
+        // console.log('Doğru Giriş');
+        // mesaj('Doğru Giriş Yaptınız👍');
+        sagKutu('Doğuru Giriş Yaptınız👍','success')
     } else {
         console.log('Yanlış bilgiler');
-        mesaj('Lütfen bilgilerinizi kontrol ediniz❗');
-        
+        // mesaj('Lütfen bilgilerinizi kontrol ediniz❗');
+        sagKutu('Lütfen bilgilerinizi kontrol ediniz❗','error');
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then((json) => {
-            const adlar = json.map(user => user.username); // Sadece "ad" kısmını al
-            console.log(adlar);
-        })
+            .then(response => response.json())
+            .then((json) => {
+                const adlar = json.map(user => user.username); // Sadece "ad" kısmını al
+                console.log(adlar);
+            })
 
     }
 }
 
 
-function mesaj(mesaj_){
+function mesaj(mesaj_) {
     mesajElement.textContent = mesaj_; // Değişken adı güncellendi
 }
